f38be3984708cf94df021e38e4cd703096ed7fae
Cristian andres Farias farias <cristian_fariass@hotmail.com>
2020-09-15 11:09:49 -0300

    update username database

--- src/keys.ts
+++ src/keys.ts
@@ -1,9 +1,9 @@
 export default{
     database:{
         host:'190.114.253.138',
-        user:'root',
+        user:'zelta',
         password:'123456',
-        database:'zeltasto_sellado'
-                        
+        database:'zeltasto_sellado'  
+                              
     }
 }
